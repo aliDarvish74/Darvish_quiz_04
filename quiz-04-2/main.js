a0a4c7abfb62988e28f547d088fc9e5162b7b309
Ali Darvish <ali.darvish.sf@gmail.com>
2023-01-27 10:47:20 +0330

    Solve quiz_04_01

--- quiz-04-2/main.js
+++ quiz-04-2/main.js
@@ -1,8 +1,8 @@
-const tableHead = document.querySelector('thead');
-const tableBody = document.querySelector('tbody');
+const tableHead = document.querySelector("thead");
+const tableBody = document.querySelector("tbody");
 
-const renderTable = () => {
-	tableHead.innerHTML = `
+const renderTable = (sortKey = null, order) => {
+  tableHead.innerHTML = `
 		<tr>
 			<th scope="col">No.</th>
 			<th scope="col">Tour ID</th>
@@ -16,9 +16,31 @@ const renderTable = () => {
 			<th scope="col">Ratings Quantity</th>
 		</tr>`;
 
-	let rowCount = 1;
-	for (const tour of tours) {
-		tableBody.innerHTML += `
+  if (!!sortKey) {
+    if (order === "dec") {
+      tours.sort((a, b) => {
+        return b[sortKey]
+          .toString()
+          .localeCompare(a[sortKey].toString(), undefined, {
+            numeric: true,
+            sensivity: false,
+          });
+      });
+    } else {
+      tours.sort((a, b) => {
+        return a[sortKey]
+          .toString()
+          .localeCompare(b[sortKey].toString(), undefined, {
+            numeric: true,
+            sensivity: false,
+          });
+      });
+    }
+  }
+
+  let rowCount = 1;
+  for (const tour of tours) {
+    tableBody.innerHTML += `
 			<tr>
 				<th scope="row">${rowCount}</th>
 				<td>${tour.id}</td>
@@ -32,8 +54,28 @@ const renderTable = () => {
 				<td>${tour.ratingsQuantity}</td>
 			</tr>`;
 
-		rowCount += 1;
-	}
+    rowCount += 1;
+  }
 };
 
-renderTable();
+const query = window.location.search;
+if (!query) {
+  renderTable();
+} else if (query.includes("sort")) {
+  let sortKey = query
+    .substring(1)
+    .split("&")
+    .find((item) => item.includes("sort"))
+    .split("=")[1];
+  let order = "acc";
+  if (sortKey[0] === "-") {
+    sortKey = sortKey.substring(1);
+    order = "dec";
+  }
+  console.log(sortKey);
+  if (Object.keys(tours[0]).includes(sortKey)) {
+    renderTable(sortKey, order);
+  }
+} else {
+  renderTable();
+}
