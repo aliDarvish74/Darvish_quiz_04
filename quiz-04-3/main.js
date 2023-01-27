34ad3b73778c3c99f034ee57f2f831335fef3df4
Ali Darvish <ali.darvish.sf@gmail.com>
2023-01-27 11:20:28 +0330

    Solve quiz_04_03

--- quiz-04-3/main.js
+++ quiz-04-3/main.js
@@ -1,39 +1,60 @@
-const tableHead = document.querySelector('thead');
-const tableBody = document.querySelector('tbody');
+const tableHead = document.querySelector("thead");
+const tableBody = document.querySelector("tbody");
 
-const renderTable = () => {
-	tableHead.innerHTML = `
+const renderTable = (filterKey = null) => {
+  const headers = Object.keys(tours[0]);
+
+  tableHead.innerHTML = `
 		<tr>
-			<th scope="col">No.</th>
-			<th scope="col">Tour ID</th>
-			<th scope="col">Name</th>
-			<th scope="col">Location</th>
-			<th scope="col">Price</th>
-			<th scope="col">Group Size</th>
-			<th scope="col">difficulty</th>
-			<th scope="col">duration</th>
-			<th scope="col">Ratings Average</th>
-			<th scope="col">Ratings Quantity</th>
+		<th scope="col">No.</th>
+			${headers
+        .map((head) => {
+          console.log(head);
+          console.log(filterKey);
+          if (!!filterKey && filterKey.includes(head)) {
+            return;
+          }
+          return `<th scope="col">${head}</th>`;
+        })
+        .join("")}
+			
 		</tr>`;
 
-	let rowCount = 1;
-	for (const tour of tours) {
-		tableBody.innerHTML += `
+  let rowCount = 1;
+  for (const tour of tours) {
+    tableBody.innerHTML += `
 			<tr>
 				<th scope="row">${rowCount}</th>
-				<td>${tour.id}</td>
-				<td>${tour.name}</td>
-				<td>${tour.location}</td>
-				<td>${tour.price}</td>
-				<td>${tour.maxGroupSize}</td>
-				<td>${tour.difficulty}</td>
-				<td>${tour.duration}</td>
-				<td>${tour.ratingsAverage}</td>
-				<td>${tour.ratingsQuantity}</td>
+				${Object.entries(tour)
+          .map(([key, value]) => {
+            if (!!filterKey && filterKey.includes(key)) {
+              return;
+            }
+
+            return `<th scope="row">${value}</th>`;
+          })
+          .join("")}
 			</tr>`;
 
-		rowCount += 1;
-	}
+    rowCount += 1;
+  }
 };
 
-renderTable();
+const query = window.location.search;
+
+if (!query) {
+  renderTable();
+} else if (query.includes("fields")) {
+  let filterKey = query
+    .substring(1)
+    .split("&")
+    .find((item) => item.includes("fields"))
+    .split("=")[1]
+    .split(",")
+    .filter((item) => item[0] === "-")
+    .map((el) => el.substring(1));
+
+  renderTable(filterKey);
+} else {
+  renderTable();
+}
